c1c9dc2b2722fb26497c0873b5ca6a6f4536d74c
ettapranathi <pranathireddyetta@gmail.com>
2022-11-17 11:12:21 +0530

    second commit

---
+++ src/main/java/SEGroup/MavenJavaProject1/App.java
@@ -0,0 +1,13 @@
+package SEGroup.MavenJavaProject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is the first Java Project in Eclipse" );
+    }
+}
