87b0cc264093dee1bbe65347ae6326d303a952e3
ettapranathi <pranathireddyetta@gmail.com>
2022-11-27 17:18:02 +0530

    First commit to java project

--- src/main/java/SEGroup/MavenJavaProject1/App.java
+++ src/main/java/SEGroup/MavenJavaProject1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is the first Java Project in Eclipse" );
+        System.out.println( "This is the first Maven Java Project by pranathi" );
     }
 }
